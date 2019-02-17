f621c8ad2d3b972170abc119ce393f3cb8431638
Eden Salmon <admin@edensa.space>
2019-02-18 00:54:32 +0200

    Added input polling.

---
+++ GameEngine/src/Platform/Windows/WindowsInput.cpp
@@ -0,0 +1,46 @@
+#include "ngpch.h"
+#include "WindowsInput.h"
+
+#include "Engine/Application.h"
+#include <GLFW/glfw3.h>
+
+namespace engine
+{
+
+	Input* Input::s_Instance = new WindowsInput();
+
+	bool WindowsInput::IsKeyPressedImpl(int keycode)
+	{
+		GLFWwindow* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetKey(window, keycode);
+		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+
+	bool WindowsInput::IsMouseButtonPressedImpl(int button)
+	{
+		GLFWwindow* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetMouseButton(window, button);
+		return state == GLFW_PRESS;
+	}
+
+	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	{
+		GLFWwindow* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+		return { static_cast<float>(xpos), static_cast<float>(ypos) };
+	}
+
+	float WindowsInput::GetMouseXImpl()
+	{
+		auto[x, y] = GetMousePositionImpl();
+		return x;
+	}
+
+	float WindowsInput::GetMouseYImpl()
+	{
+		auto[x, y] = GetMousePositionImpl();
+		return y;
+	}
+
+}

455fe929fda98f9b00590f9e1c73b03a03d23da4
Eden Salmon <admin@edensa.space>
2019-12-14 14:52:27 +0200

    Shaders can now be read from files

--- GameEngine/src/Engine/Renderer/Shader.cpp
+++ GameEngine/src/Engine/Renderer/Shader.cpp
@@ -7,6 +7,22 @@
 
 namespace engine
 {
+
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			ENGINE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+			return nullptr;
+		case RendererAPI::API::OpenGL:
+			return new OpenGLShader(filepath);
+		default:
+			ENGINE_CORE_ASSERT(false, "unknown RendererAPI!");
+			return nullptr;
+		}
+	}
+	
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())

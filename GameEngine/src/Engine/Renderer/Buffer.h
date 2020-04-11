17dac3c85f0dcc7df7ea0f86d64e5ec72d4e48ed
Eden Salmon <admin@edensa.space>
2020-04-12 01:28:55 +0300

    Fix Build warnings to do with BufferElement Offset Type

--- GameEngine/src/Engine/Renderer/Buffer.h
+++ GameEngine/src/Engine/Renderer/Buffer.h
@@ -35,9 +35,11 @@ namespace engine
 		std::string Name;
 		ShaderDataType Type;
 		uint32_t Size;
-		uint32_t Offset;
+		size_t Offset;
 		bool Normalized;
 
+		BufferElement() = default;
+		
 		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false)
 			: Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0), Normalized(normalized)
 		{
@@ -93,7 +95,7 @@ namespace engine
 	private:
 		void CalculateOffsetsAndStride()
 		{
-			uint32_t offset = 0;
+			size_t offset = 0;
 			m_Stride = 0;
 			for (auto& element : m_Elements)
 			{

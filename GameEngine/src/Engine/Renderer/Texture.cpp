9af28a447edfacad22f1dde29adae005227d3047
Eden Salmon <admin@edensa.space>
2020-04-13 16:21:36 +0300

    Renderer2D now uses a single shader to render both textured and non-textured quads

--- GameEngine/src/Engine/Renderer/Texture.cpp
+++ GameEngine/src/Engine/Renderer/Texture.cpp
@@ -6,6 +6,21 @@
 
 namespace engine
 {
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			ENGINE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+			return nullptr;
+		case RendererAPI::API::OpenGL:
+			return CreateRef<OpenGLTexture2D>(width, height);
+		default:
+			ENGINE_CORE_ASSERT(false, "unknown RendererAPI!");
+			return nullptr;
+		}
+	}
+	
 	Ref<Texture2D> Texture2D::Create(const std::string& path)
 	{
 		switch (Renderer::GetAPI())
@@ -14,7 +29,7 @@ namespace engine
 			ENGINE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
 			return nullptr;
 		case RendererAPI::API::OpenGL:
-			return std::make_shared<OpenGLTexture2D>(path);
+			return CreateRef<OpenGLTexture2D>(path);
 		default:
 			ENGINE_CORE_ASSERT(false, "unknown RendererAPI!");
 			return nullptr;
--- GameEngine/src/Platform/OpenGL/OpenGLTexture.h
+++ GameEngine/src/Platform/OpenGL/OpenGLTexture.h
@@ -2,17 +2,22 @@
 
 #include "Engine/Renderer/Texture.h"
 
+#include <glad/glad.h>
+
 namespace engine
 {
 	
 	class OpenGLTexture2D : public Texture2D
 	{
 	public:
+		OpenGLTexture2D(uint32_t width, uint32_t height);
 		OpenGLTexture2D(const std::string& path);
 		virtual ~OpenGLTexture2D();
 
 		virtual uint32_t GetWidth() const override { return m_Width; }
 		virtual uint32_t GetHeight() const override { return m_Height; }
+
+		void SetData(void* data, uint32_t size) override;
 		
 		virtual void Bind(uint32_t slot = 0) const override;
 		
@@ -21,6 +26,8 @@ namespace engine
 		uint32_t m_Width;
 		uint32_t m_Height;
 		uint32_t m_RendererID;
+		GLenum m_InternalFormat;
+		GLenum m_DataFormat;
 
 	};
 

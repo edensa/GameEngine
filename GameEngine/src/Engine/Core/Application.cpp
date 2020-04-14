e0921006287e7bde13642e5bf5bc331be3824b93
Eden Salmon <admin@edensa.space>
2020-04-14 19:39:11 +0300

    Missing iterator wrappers in LayerStack.h

--- GameEngine/src/Engine/Core/Application.cpp
+++ GameEngine/src/Engine/Core/Application.cpp
@@ -64,9 +64,9 @@ namespace engine {
 		dispatcher.Dispatch<WindowCloseEvent>(ENGINE_BIND_EVENT_FN(Application::OnWindowClose));
 		dispatcher.Dispatch<WindowResizeEvent>(ENGINE_BIND_EVENT_FN(Application::OnWindowResize));
 
-		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
+		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it)
 		{
-			(*--it)->OnEvent(e);
+			(*it)->OnEvent(e);
 			if (e.Handled)
 				break;
 		}

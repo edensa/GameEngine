a9ea97ec191e8595d6b499ebb2957b3d9c9417e0
Eden Salmon <admin@edensa.space>
2020-04-14 19:58:31 +0300

    Auto deducing an availabe __FUNCSIG__ definition

--- GameEngine/src/Engine/Debug/Instrumentor.h
+++ GameEngine/src/Engine/Debug/Instrumentor.h
@@ -126,13 +126,34 @@ namespace engine
 }
 
 #if ENGINE_PROFILE
-#define ENGINE_PROFILE_BEGIN_SESSION(name, filepath)		::engine::Instrumentor::Get().BeginSession(name, filepath)
-#define ENGINE_PROFILE_END_SESSION()						::engine::Instrumentor::Get().EndSession()
-#define ENGINE_PROFILE_SCOPE(name)							::engine::InstrumentationTimer timer##__LINE__(name)
-#define ENGINE_PROFILE_FUNCTION()							ENGINE_PROFILE_SCOPE(__FUNCSIG__)
+	// Resolve which function signature macro will be used. Note that this only
+	// is resolved when the (pre)compiler starts, so the syntax highlighting
+	// could mark the wrong one in your editor!
+	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+		#define ENGINE_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__DMC__) && (__DMC__ >= 0x810)
+		#define ENGINE_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__FUNCSIG__)
+		#define ENGINE_FUNC_SIG __FUNCSIG__
+	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+		#define ENGINE_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+		#define ENGINE_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+		#define ENGINE_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+		#define ENGINE_FUNC_SIG __func__
+	#else
+		#define ENGINE_FUNC_SIG "ENGINE_FUNC_SIG unknown!"
+	#endif
+
+	#define ENGINE_PROFILE_BEGIN_SESSION(name, filepath)		::engine::Instrumentor::Get().BeginSession(name, filepath)
+	#define ENGINE_PROFILE_END_SESSION()						::engine::Instrumentor::Get().EndSession()
+	#define ENGINE_PROFILE_SCOPE(name)							::engine::InstrumentationTimer timer##__LINE__(name)
+	#define ENGINE_PROFILE_FUNCTION()							ENGINE_PROFILE_SCOPE(ENGINE_FUNC_SIG)
 #else
-#define ENGINE_PROFILE_BEGIN_SESSION(name, filepath)
-#define ENGINE_PROFILE_END_SESSION()
-#define ENGINE_PROFILE_SCOPE(name)
-#define ENGINE_PROFILE_FUNCTION()
+	#define ENGINE_PROFILE_BEGIN_SESSION(name, filepath)
+	#define ENGINE_PROFILE_END_SESSION()
+	#define ENGINE_PROFILE_SCOPE(name)
+	#define ENGINE_PROFILE_FUNCTION()
 #endif // !ENGINE_PROFILE

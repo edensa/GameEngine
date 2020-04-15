82b2a8eafa35a586b3b4bac1a825e0b0ce9d79de
Eden Salmon <admin@edensa.space>
2020-04-15 14:42:39 +0300

    Updated Sandbox2D

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -45,10 +45,10 @@ void Sandbox2D::OnUpdate(engine::Timestep ts)
 	{
 		ENGINE_PROFILE_SCOPE("Renderer Draw");
 		engine::Renderer2D::BeginScene(m_CameraController.GetCamera());
-
-		engine::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+		
+		engine::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
 		engine::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		engine::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.f, glm::vec4(1.0f));
+		engine::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.f);
 		engine::Renderer2D::EndScene();
 	}
 }

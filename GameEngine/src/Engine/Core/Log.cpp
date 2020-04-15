4098b4d08cc9aaa3b5a8aa0abd1dca98b65bd8fc
Eden Salmon <admin@edensa.space>
2020-04-15 14:34:00 +0300

    Log messages are now additionally sent to Engine.log file

--- GameEngine/src/Engine/Core/Log.cpp
+++ GameEngine/src/Engine/Core/Log.cpp
@@ -2,6 +2,7 @@
 #include "Engine/Core/Log.h"
 
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include <spdlog/sinks/basic_file_sink.h>
 
 using std::shared_ptr;
 
@@ -12,10 +13,21 @@ namespace engine {
 
 	void Log::Init()
 	{
-		spdlog::set_pattern("%^[%T] %n: %v%$");
-		s_CoreLogger = spdlog::stdout_color_mt("ENGINE");
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Engine.log", true));
+
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+		s_CoreLogger = std::make_shared<spdlog::logger>("ENGINE", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_CoreLogger);
 		s_CoreLogger->set_level(spdlog::level::trace);
-		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_CoreLogger->flush_on(spdlog::level::trace);
+
+		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_ClientLogger);
 		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
 	}
 }
--- GameEngine/src/Engine/Core/Log.h
+++ GameEngine/src/Engine/Core/Log.h
@@ -23,15 +23,15 @@ namespace engine
 }
 
 // Core log macros
-#define ENGINE_CORE_TRACE(...)   ::engine::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define ENGINE_CORE_INFO(...)    ::engine::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define ENGINE_CORE_WARN(...)    ::engine::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define ENGINE_CORE_ERROR(...)   ::engine::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define ENGINE_CORE_CRITICAL(...)   ::engine::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#define ENGINE_CORE_TRACE(...)		::engine::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define ENGINE_CORE_INFO(...)		::engine::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define ENGINE_CORE_WARN(...)		::engine::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define ENGINE_CORE_ERROR(...)		::engine::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define ENGINE_CORE_CRITICAL(...)	::engine::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // client log macros
-#define ENGINE_TRACE(...)        ::engine::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define ENGINE_INFO(...)         ::engine::Log::GetClientLogger()->info(__VA_ARGS__)
-#define ENGINE_WARN(...)         ::engine::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define ENGINE_ERROR(...)        ::engine::Log::GetClientLogger()->error(__VA_ARGS__)
-#define ENGINE_CRITICAL(...)        ::engine::Log::GetClientLogger()->critical(__VA_ARGS__)
+#define ENGINE_TRACE(...)			::engine::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define ENGINE_INFO(...)			::engine::Log::GetClientLogger()->info(__VA_ARGS__)
+#define ENGINE_WARN(...)			::engine::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define ENGINE_ERROR(...)			::engine::Log::GetClientLogger()->error(__VA_ARGS__)
+#define ENGINE_CRITICAL(...)		::engine::Log::GetClientLogger()->critical(__VA_ARGS__)

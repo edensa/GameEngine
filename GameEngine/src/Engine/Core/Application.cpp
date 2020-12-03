bdca15aa19db44e78a3b560591fee36463f86b47
Eden Salmon <admin@edensa.space>
2020-12-04 01:13:23 +0200

    Imgui handling mouse and keyboards events

--- GameEngine/src/Engine/Core/Application.cpp
+++ GameEngine/src/Engine/Core/Application.cpp
@@ -66,9 +66,9 @@ namespace engine {
 
 		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it)
 		{
-			(*it)->OnEvent(e);
 			if (e.Handled)
 				break;
+			(*it)->OnEvent(e);
 		}
 	}
 
--- GameEngine/src/Engine/ImGui/ImGuiLayer.cpp
+++ GameEngine/src/Engine/ImGui/ImGuiLayer.cpp
@@ -65,6 +65,13 @@ namespace engine
 		ImGui::DestroyContext();
 	}
 
+	void ImGuiLayer::OnEvent(Event& e)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		e.Handled |= e.IsInCategory(EventCategoryMouse) && io.WantCaptureMouse;
+		e.Handled |= e.IsInCategory(EventCategoryKeyboard) && io.WantCaptureKeyboard;
+	}
+
 	void ImGuiLayer::Begin()
 	{
 		ENGINE_PROFILE_FUNCTION();

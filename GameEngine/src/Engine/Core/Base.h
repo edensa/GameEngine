857076b01a031eb411a4cfd6e8cdc46d8029e71c
Eden Salmon <admin@edensa.space>
2020-12-12 23:54:48 +0200

    replaced std::bind in HZ_BIND_EVENT_FN macro

--- GameEngine/src/Engine/Core/Base.h
+++ GameEngine/src/Engine/Core/Base.h
@@ -68,7 +68,7 @@
 
 #define BIT(b) (1 << b)
 
-#define ENGINE_BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
+#define ENGINE_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace engine
 {

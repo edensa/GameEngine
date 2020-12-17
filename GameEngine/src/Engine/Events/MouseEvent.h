f18c51d6887e6c940f0f0f359498278be53cc384
Eden Salmon <admin@edensa.space>
2020-12-17 14:53:50 +0200

    Add EventCategoryMouseButton to MouseButtonEvent

--- GameEngine/src/Engine/Events/MouseEvent.h
+++ GameEngine/src/Engine/Events/MouseEvent.h
@@ -24,7 +24,7 @@ namespace engine
 		}
 
 		EVENT_CLASS_TYPE(MouseMoved)
-		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput | EventCategoryMouseButton)
 
 	private:
 		float m_MouseX;

664f02dd39a2469a957446978dbf8d54f6b2cb6e
Eden Salmon <admin@edensa.space>
2022-03-22 19:09:24 +0200

    update to vs2022

--- scripts/setup.py
+++ scripts/setup.py
@@ -17,4 +17,4 @@ if (not get_vulkan.check_vulkan_sdk_debug_libs()):
     print("Vulkan SDK debug libs not found.")
 
 print("Running premake...")
-subprocess.call(["vendor/premake/bin/premake5.exe", "vs2019"])
+subprocess.call(["vendor/premake/bin/premake5.exe", "vs2022"])
